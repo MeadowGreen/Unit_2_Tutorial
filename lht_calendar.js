5cdcacf4fe3e4ebd2d5e931be3200233587a19bb
Meadow Green <mgreen161@west-mec.org>
2020-02-03 13:12:42 -0700

    finished

--- lht_calendar.js
+++ lht_calendar.js
@@ -31,7 +31,7 @@
 */
 
 //set the date displayed in the calendar
-var thisDay = new Date("August 24, 2018");
+var thisDay = new Date();
 
 //write the calendar to the element with the id "calendar"
 document.getElementById("calendar").innerHTML = createCalendar(thisDay);
@@ -39,14 +39,14 @@ document.getElementById("calendar").innerHTML = createCalendar(thisDay);
 function createCalendar(calDate){
     var calendarHTML = "<table id = 'calendar_table'>" ;
     calendarHTML += calCaption(calDate);
+    calendarHTML += calWeekdayRow();
+    calendarHTML += calDays(calDate);
     calendarHTML += "</table>";
     return calendarHTML;
 }
 
 function calCaption(calDate){
     var monthName = ["January" , "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
-
 
     //this determines the current month 
     var thisMonth = calDate.getMonth();
@@ -58,3 +58,79 @@ function calCaption(calDate){
     return "<caption>" + monthName[thisMonth] + " " + thisYear + "</caption>";
 }
 
+// function to write a table row of weekday abbreviations
+function calWeekdayRow(){
+   //array to hold the weekday abbreviations
+   var dayName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+   var rowHTML = "<tr>";
+
+   //look throught the dayName array 
+   for(var i = 0; i < dayName.length; i++){
+      rowHTML += "<th class='calendar_weekdays'>" + dayName[i] + "</th>";
+   }
+
+   rowHTML += "</tr>";
+   return rowHTML;
+}
+
+//function to calculate the number of days in the month
+function daysInMonth(calDate){
+   var dayCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+   //extract the four digit year abd month values
+   var thisYear = calDate.getFullYear();
+
+   //revise the dayCount for leap years
+   if(thisYear % 4 === 0){
+      if((thisYear % 100 != 0) || (thisYear % 400 !=0 )){
+         dayCount[1] = 29;
+      }
+      dayCount[1] = 29;
+   }
+
+   var thisMonth = calDate.getMonth();
+
+   //return the number of days in the month
+   return dayCount[thisMonth];
+ 
+}
+
+//function to write table rows for each day of the month 
+function calDays(calDate){
+   //determine the starting day 
+   var day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+   var weekDay = day.getDay();
+
+   //write blank spaces preceding the starting day
+   var htmlCode = "<tr>"; 
+   for(var i= 0; i<weekDay; i++){
+      htmlCode+="<td></td>";
+   }
+
+
+   //write cells for each day of the month 
+   var totalDays = daysInMonth(calDate);
+
+   var highlightedDay = calDate.getDate();
+
+   for(var i = 1; i <= totalDays; i++){
+      day.setDate(i);
+      weekDay = day.getDay();
+      if(weekDay === 0){
+         htmlCode += "<tr>";
+      }
+
+
+      if(i === highlightedDay){
+         htmlCode += "<td class= 'calendar_dates' id='calendar_today'>" + i +  dayEvent[i] + "</td>";
+      } else{
+         htmlCode += "<td class= 'calendar_dates'>" + i + dayEvent[i] + "</td>";
+      }
+
+
+      if(weekDay === 6){
+         htmlCode += "</tr>";
+      }
+   }
+   return htmlCode;
+}
